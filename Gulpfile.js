c220c1eede3bed917774f1c9353b80dc138a4523
Alexander <alexander@dpi.nl>
2021-12-02 10:16:16 +0100

    Set default breakpoints for functions and mixins

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,13 +4,12 @@
 var gulp = require('gulp');
 var pipeline = require('readable-stream').pipeline;
 var rename = require('gulp-rename');
-var sass = require('gulp-sass');
+let sass = require('gulp-sass')(require('node-sass'));
 var sourcemaps = require('gulp-sourcemaps');
 
 sass.compiler = require('node-sass');
 
 gulp.task('sass', function () {
-    // TODO: Bring back sourcemap without breaking the pipeline.
     return gulp.src('./scss/**/*.scss')
         .pipe(sass({outputStyle: 'expanded'}))
         .pipe(gulp.dest('./css'))
